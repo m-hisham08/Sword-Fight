efeabf7ddf7052daf3233db6db7b75b1032477ce
HP <HP@DESKTOP-PDMJB0F>
2023-03-31 10:02:56 -0700

    Done with mainmenu for now

--- src/MainMenuState.cpp
+++ src/MainMenuState.cpp
@@ -5,17 +5,33 @@ void MainMenuState::initButtons()
 {
 	this->button = new mygui::Buttons(
 		sf::Vector2f(300.f, 150.f),
-		this->window.getSize().x / 5, this->window.getSize().y / 2,
-		"res/fonts/ARCADECLASSIC.TTF", "PRESS ANY BUTTON TO START!",
-		12, sf::Color::Black
+		this->window.getSize().x / 4, this->window.getSize().y / 1.5,
+		"res/fonts/ARCADECLASSIC.TTF", "PRESS  ANY  BUTTON  TO  START!",
+		36
 	);
+}
+
+void MainMenuState::initBackgrounds()
+{
+	bg1Text = new sf::Texture();
+	bg1Text->loadFromFile("res/backgrounds/oakwood/background/1366x768/background_layer_1.png");
+	bg1Sprite = new sf::Sprite(*bg1Text);
+
+	bg2Text = new sf::Texture();
+	bg2Text->loadFromFile("res/backgrounds/oakwood/background/1366x768/background_layer_2.png");
+	bg2Sprite = new sf::Sprite(*bg2Text);
+
+	bg3Text = new sf::Texture();
+	bg3Text->loadFromFile("res/backgrounds/oakwood/background/1366x768/background_layer_3.png");
+	bg3Sprite = new sf::Sprite(*bg3Text);
+
 }
 
 MainMenuState::MainMenuState(sf::RenderWindow& window):State(window)
 {
 	std::cout << "Main menu state pushed! \n";
+	this->initBackgrounds();
 	this->initButtons();
-	
 }
 
 void MainMenuState::update(const float& dt)
@@ -25,6 +41,8 @@ void MainMenuState::update(const float& dt)
 
 void MainMenuState::render()
 {
-	this->window.clear(sf::Color::Red);
+	this->window.draw(*bg1Sprite);
+	this->window.draw(*bg2Sprite);
+	this->window.draw(*bg3Sprite);
 	this->button->render(this->window);
 }
--- src/MainMenuState.h
+++ src/MainMenuState.h
@@ -6,9 +6,19 @@ class MainMenuState :
     public State
 {
 private:
+    sf::Texture* bg1Text;
+    sf::Sprite* bg1Sprite;
+
+    sf::Texture* bg2Text;
+    sf::Sprite* bg2Sprite;
+
+    sf::Texture* bg3Text;
+    sf::Sprite* bg3Sprite;
+
     mygui::Buttons* button;
 
     void initButtons();
+    void initBackgrounds();
     
 public:
     MainMenuState(sf::RenderWindow& window);
